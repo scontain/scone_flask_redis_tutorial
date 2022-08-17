f67c76280e120d0bf90002d20a2e692b5b30e1b1
Christof Fetzer <christof.fetzer@scontain.com>
2022-08-17 15:25:29 +0200

    switched to new run.sh script

---
+++ check_prerequisites.sh
@@ -0,0 +1,117 @@
+#!/usr/bin/env bash
+
+set -e
+
+export RED='\e[31m'
+export BLUE='\e[34m'
+export ORANGE='\e[33m'
+export NC='\e[0m' # No Color
+
+
+# print an error message on an error exiting
+trap 'last_command=$current_command; current_command=$BASH_COMMAND' DEBUG
+trap 'if [ $? -ne 0 ]; then echo "${RED}\"${last_command}\" command failed - exiting.${NC}"; fi' EXIT
+
+function error_exit() {
+  trap 'echo -e  "${RED}Exiting with error.${NC}"' EXIT
+  exit 1
+}
+
+echo -e "${BLUE}Checking that we have access to sconectl${NC}"
+if ! command -v sconectl &> /dev/null
+then
+    echo -e "${ORANGE}No sconectl found! Installing sconectl!${NC}"
+    echo -e "${ORANGE}Ensuring that we have access to a new Rust installation${NC}"
+
+    if ! command -v rustup &> /dev/null
+    then
+        echo -e "${ORANGE}No Rust found! Installing Rust!${NC}"
+        curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
+    else
+        echo -e "${ORANGE}Ensuring Rust is up to date${NC}"
+        rustup update
+    fi
+    if ! command -v cc &> /dev/null
+    then
+       echo -e "${RED} No (g)cc found! Installing sconectl is likely to fail!${NC}"
+       echo -e "${ORANGE} On Ubuntu, you can install gcc as follows: sudo apt-get install -y build-essential ${NC}"
+    fi
+    cargo install sconectl
+fi
+
+
+echo -e "${BLUE}Checking that we have access to docker${NC}"
+if ! command -v docker &> /dev/null
+then
+    echo -e "${RED}No docker found! You need to install docker or podman. EXITING.${NC}"
+    error_exit
+fi
+
+echo -e "${BLUE}Checking that we run applications with docker without sudo${NC}"
+if ! docker run -it hello-world &> /dev/null
+then
+    echo -e "${RED}Docker does not seem to run."
+    echo -e "Please ensure that you can run docker without sudo: https://docs.docker.com/engine/install/linux-postinstall/." 
+    echo -e "Ensure that command 'docker run -it hello-world' runs without problems${NC}"
+    error_exit
+fi
+
+echo -e "${BLUE}Checking that we can run container images for linux/amd64${NC}"
+if ! docker run --platform linux/amd64 -it hello-world &> /dev/null
+then
+    echo -e "${RED}Docker does not seem to support argument '--plaform linux/amd64'"
+    echo -e "Please ensure that you can run the latest version of docker (i.e.,  API version >= 1.40)" 
+    VERSIONS=$(docker version | grep "API version" | awk '{ print $3}')
+    for i in $VERSIONS ; do
+      if [[ "$i" < "1.40" ]] ; then
+        echo "Your docker API version is only '$i'."
+        error_exit
+      fi
+    done
+    echo -e "Please determine the version number with 'docker version' and update.${NC}"
+    error_exit
+fi
+
+echo -e "${BLUE}Checking that we the CPU has all necessary CPU features enabled${NC}"
+if ! docker run --platform linux/amd64 -it registry.scontain.com:5050/cicd/check_cpufeatures:latest &> /dev/null
+then
+    echo -e "${RED}Docker does not seem to support all CPU features.${NC}"
+    echo -e "- ${ORANGE}Assuming you do not run on a modern Intel CPU. Please ensure that you pass the following options to qemu: -cpu qemu64,+ssse3,+sse3,+sse4.1,+sse4.2,+rdrand,+popcnt,+xsave,+aes${NC}" 
+    error_exit
+fi
+
+echo -e "${BLUE}Checking that we have access to kubectl${NC}"
+if ! command -v kubectl &> /dev/null
+then
+    echo -e "${RED}Command 'kubectl' not found!${NC}"
+    echo -e "- ${ORANGE}Please install - see https://kubernetes.io/docs/tasks/tools/${NC}"
+    error_exit
+fi
+
+echo -e "${BLUE}Checking that we have access to helm${NC}"
+if ! command -v helm &> /dev/null
+then
+    echo -e "${RED}Command 'helm' not found!${NC}"
+    echo -e "- ${ORANGE}Please install - see https://helm.sh/docs/intro/install/${NC}"
+    error_exit
+fi
+
+echo -e "${BLUE}Checking that we have access to envsubst${NC}"
+if ! command -v envsubst &> /dev/null
+then
+    echo -e "${RED}Command 'envsubst' not found!${NC}"
+    echo -e "- ${ORANGE}Please install envsubst${NC}"
+    error_exit
+fi
+
+
+echo -e "${BLUE}Checking that directory $HOME/.scone exits${NC}"
+if [[ ! -e "$HOME/.scone" ]] ; then 
+  echo -e " - Creating directory $HOME/.scone"
+  mkdir -p "$HOME/.scone" ||  ( echo -e "${RED}Failed to create $HOME/.scone${NC}" ; error_exit)
+fi
+
+echo -e "${BLUE}Making sure that $HOME/.scone can be written by all. ${NC}"
+echo -e "   ${BLUE}This is needed since we might have a different user ID inside of a container${NC}"
+chmod 0777 "$HOME/.scone" ||  ( echo -e "${RED}Failed to create $HOME/.scone$.\n Maybe, run 'sudo chmod 0777 $HOME/.scone'{NC}" ; error_exit)
+
--- run.sh
+++ run.sh
@@ -2,93 +2,158 @@
 
 set -e
 
-RED="\e[31m"
-BLUE='\e[34m'
-ORANGE='\e[33m'
-NC='\e[0m' # No Color
 
-RELEASE="flaskredis"
+export RED='\e[31m'
+export BLUE='\e[34m'
+export ORANGE='\e[33m'
+export NC='\e[0m' # No Color
+
+source release.sh || true # get release name
+
+DEFAULT_NAMESPACE="" # Default Kubernetes namespace to use
+APP_IMAGE_REPO=${APP_IMAGE_REPO:=""} # Must be defined!
 
 # print an error message on an error exit
 trap 'last_command=$current_command; current_command=$BASH_COMMAND' DEBUG
 trap 'if [ $? -ne 0 ]; then echo -e "${RED}\"${last_command}\" command failed - exiting.${NC}"; fi' EXIT
 
-echo -e "${BLUE}Checking that we have access to sconectl${NC}"
+help_flag="--help"
+ns_flag="--namespace"
+ns_short_flag="-n"
+repo_flag="--image_repo"
+repo_short_flag="-i"
+verbose_flag="-v"
+verbose=""
+release_flag="--release"
+release_short_flag="-r"
+verbose=""
 
-if ! command -v sconectl &> /dev/null
-then
-    echo -e "${ORANGE}No sconectl found! Installing sconectl!${NC}"
-    echo -e "${ORANGE}Ensuring that we have access to a new Rust installation${NC}"
+ns="$DEFAULT_NAMESPACE"
+repo="$APP_IMAGE_REPO"
+release="$RELEASE"
 
-    if ! command -v rustup &> /dev/null
-    then
-        echo -e "${ORANGE}No Rust found! Installing Rust!${NC}"
-        curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
-    else
-        echo -e "${ORANGE}Ensuring Rust is up to date${NC}"
-        rustup update
-    fi
+error_exit() {
+  trap '' EXIT
+  echo -e "${RED}$1${NC}" 
+  exit 1
+}
 
-    cargo install sconectl
+usage ()
+{
+  echo ""
+  echo "Usage:"
+  echo "    run.sh [$ns_flag <kubernetes-namespace>] [$repo_flag <image repo>] [$release_flag <release name>] [$verbose_flag] [$help_flag]"
+  echo ""
+  echo ""
+  echo "Builds the application described in service.yaml and mesh.yaml and deploys"
+  echo "it into your kubernetes cluster."
+  echo ""
+  echo "Options:"
+  echo "    $ns_short_flag | $ns_flag"
+  echo "                  The namespace in which the application should be deployed on the cluster."
+  echo "                  Default value: \"$DEFAULT_NAMESPACE\""
+  echo "    $release_flag | $release_short_flag"
+  echo "                  The helm release name of the application. "
+  echo "                  Default value defined in file 'release.sh': RELEASE=\"$RELEASE\""
+  echo "    $repo_short_flag | $repo_flag"
+  echo "                  Container image repository to use for pushing the generated confidential image"
+  echo "                  Default value is defined by environment variable:"
+  echo "                    export APP_IMAGE_REPO=\"$APP_IMAGE_REPO\""
+  echo "    $verbose_flag"
+  echo "                  Enable verbose output"
+  echo "    $help_flag"
+  echo "                  Output this usage information and exit."
+  return
+}
+
+##### Parsing arguments
+
+while [[ "$#" -gt 0 ]]; do
+  case $1 in
+    ${ns_flag} | ${ns_short_flag})
+      ns="$2"
+      if [ ! -n "${ns}" ]; then
+        usage
+        error_exit "Error: The namespace '$ns' is invalid."
+      fi
+      shift # past argument
+      shift || true # past value
+      ;;
+    ${release_flag} | ${release_short_flag})
+      release="$2"
+      if [ ! -n "${release}" ]; then
+        usage
+        error_exit "Error: The release name '$release' is invalid."
+      fi
+      shift # past argument
+      shift || true # past value
+      ;;
+    ${verbose_flag})
+      verbose="-vvvvvvvv"
+      shift # past argument
+      ;;
+    $help_flag)
+      usage
+      exit 0
+      ;;
+    *)
+      usage
+      error_exit "Error: Unknown parameter passed: $1";
+      ;;
+  esac
+done
+
+if [ ! -n "${ns}" ]; then
+    namespace_arg=""
+else
+    namespace_arg="${ns_flag} ${ns} "
 fi
 
-echo -e "${BLUE}Checking that we have access to docker${NC}"
-
-if ! command -v docker &> /dev/null
-then
-    echo -e "${RED}No docker found! You need to install docker or podman. EXITING.${NC}"
-    exit 1
+if [  "${repo}" == "" ]; then
+    usage
+    error_exit  "Error: You must specify a repo."
 fi
 
-if ! command -v kubectl &> /dev/null
-then
-    echo -e "${RED}Command 'kubectl' not found!${NC}"
-    echo -e "- ${ORANGE}Please install - see https://kubernetes.io/docs/tasks/tools/${NC}"
-    exit 1
-fi
-
-if ! command -v helm &> /dev/null
-then
-    echo -e "${RED}Command 'helm' not found!${NC}"
-    echo -e "- ${ORANGE}Please install - see https://helm.sh/docs/helm/helm_install/${NC}"
-    exit 1
-fi
+# Check to make sure all prerequisites are installed
+./check_prerequisites.sh
 
 echo -e "${BLUE}Checking that we have access to the base container image${NC}"
 
-docker inspect registry.scontain.com:5050/cicd/sconecli:latest > /dev/null 2> /dev/null || docker pull registry.scontain.com:5050/cicd/sconecli:latest > /dev/null 2> /dev/null || { 
-    echo -e "${RED}You must get access to image `cicd/sconecli:latest`." 
-    echo -e "Please send email info@scontain.com to ask for access${NC}"
-    exit 1
+docker inspect registry.scontain.com:5050/sconectl/sconecli:latest > /dev/null 2> /dev/null || docker pull registry.scontain.com:5050/sconectl/sconecli:latest > /dev/null 2> /dev/null || { 
+    echo -e "${RED}You must get access to image `sconectl/sconecli:latest`.${NC}" 
+    error_exit "Please send email info@scontain.com to ask for access"
 }
 
 
 echo -e "${BLUE}let's ensure that we build everything from scratch${NC}" 
-rm -rf target
+rm -rf target || echo -e "${ORANGE} Failed to delete target directory - ignoring this! ${NC}"
 
 
 echo -e  "${BLUE}build service image:${NC} apply -f service.yaml"
-echo -e  "${BLUE} - if the push fails, add --no-push to avoid pusing the image, or${NC}"
-echo -e  "${BLUE}   change in file '${ORANGE}service.yaml${BLUE}' field '${ORANGE}build.to${BLUE}' to a container repo you have permission to push to.${NC}"
+echo -e  "${BLUE} - if the push fails, add --no-push to avoid pushing the image, or${NC}"
+echo -e  "${BLUE}   change in file '${ORANGE}service.yaml${BLUE}' field '${ORANGE}build.to${BLUE}' to a container repo to which you have permission to push.${NC}"
 
- 
-sconectl apply -f service.yaml
+envsubst < service.yaml.template > service.yaml
+
+sconectl apply -f service.yaml $verbose
 
 
 echo -e "${BLUE}build application and pushing policies:${NC} apply -f mesh.yaml"
 echo -e "${BLUE}  - this fails, if you do not have access to the SCONE CAS namespace"
 echo -e "  - update the namespace '${ORANGE}policy.namespace${NC}' to a unique name in '${ORANGE}mesh.yaml${NC}'"
 
-sconectl apply -f mesh.yaml
+envsubst < mesh.yaml.template > mesh.yaml
 
-echo -e "${BLUE}Uninstalling application in case it was previously installed:${NC} helm uninstall ${RELEASE}"
+sconectl apply -f mesh.yaml $verbose
+
+echo -e "${BLUE}Uninstalling application in case it was previously installed:${NC} helm uninstall ${namespace_args} ${RELEASE}"
 echo -e "${BLUE} - this requires that 'kubectl' gives access to a Kubernetes cluster${NC}"
 
-helm uninstall ${RELEASE} 2> /dev/null || true 
+helm uninstall $namespace_arg ${release} 2> /dev/null || true
 
-echo -e "${BLUE}install application:${NC} helm install ${RELEASE} target/helm/"
+echo -e "${BLUE}install application:${NC} helm install ${namespace_args} ${RELEASE} target/helm/"
 
-helm install ${RELEASE} target/helm/
+helm install $namespace_arg ${release} target/helm/
 
-echo -e "${BLUE}Check the logs by executing:${NC} kubectl logs ${RELEASE}<TAB>"
-echo -e "${BLUE}Uninstall by executing:${NC} helm uninstall ${RELEASE}"
+echo -e "${BLUE}Check the logs by executing:${NC} kubectl logs ${namespace_args} ${RELEASE}<TAB>"
+echo -e "${BLUE}Uninstall by executing:${NC} helm uninstall ${namespace_args} ${RELEASE}"
